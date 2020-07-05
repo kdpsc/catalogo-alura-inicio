45607b5bbf7d11af0e93053de69120c16bfb694f
kdpsc <48528728+kdpsc@users.noreply.github.com>
2020-07-05 19:44:51 -0300

    update deleta-aplicacao.sh

--- scripts/deleta-aplicacao.sh
+++ scripts/deleta-aplicacao.sh
@@ -6,4 +6,7 @@ sudo mysqldump -h localhost -u root -p12345678 alura_aws > /tmp/alura_aws-hmg-$T
 mysql -h localhost -u root -p12345678 -e "drop database alura_aws"
 mysql -h localhost -u root -p12345678 -e "create database alura_aws"
 
+sudo rm -f /var/lib/tomcat8/webapps/*.war
+sudo rm -rf /var/lib/tomcat8/webapps/loja
+sudo rm -rf /var/lib/tomcat8/webapps/alura-aws
 sudo cp target/alura-aws.war /var/lib/tomcat8/webapps/loja.war
