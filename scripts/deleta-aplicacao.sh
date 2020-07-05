688a2569a8ebe4c5351a007f10d5b4a3f13d3a69
kdpsc <48528728+kdpsc@users.noreply.github.com>
2020-07-05 20:09:38 -0300

    update deleta-aplicacao.sh

--- scripts/deleta-aplicacao.sh
+++ scripts/deleta-aplicacao.sh
@@ -3,9 +3,8 @@
 export TAG="$(date +%d-%m-%Y-%H:%M:%S)"
 
 sudo mysqldump -h localhost -u root -p12345678 alura_aws > /tmp/alura_aws-hmg-$TAG.sql
+
 mysql -h localhost -u root -p12345678 -e "drop database alura_aws" &>> /dev/null
 mysql -h localhost -u root -p12345678 -e "create database alura_aws" &>> /dev/null
 
-rm -rf /var/lib/tomcat8/webapps/*.war
-rm -rf /var/lib/tomcat8/webapps/alura-aws
-rm -rf /var/lib/tomcat8/webapps/loja
+rm -rf /var/lib/tomcat8/webapps/alura-aws*
